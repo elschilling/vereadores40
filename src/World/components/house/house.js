e4b5f45b46eb88972a9d2a63966845330e5695bd
Eduardo Schilling <elschilling@gmail.com>
2025-08-14 13:00:01 -0300

    feat: initial commit

--- src/World/components/house/house.js
+++ src/World/components/house/house.js
@@ -8,9 +8,9 @@ async function loadHouse() {
   dracoLoader.setDecoderPath('/draco/')
   const gltfLoader = new GLTFLoader()
   gltfLoader.setDRACOLoader(dracoLoader)
-  const houseData = await gltfLoader.loadAsync('/assets/models/House-c.glb')
+  const houseData = await gltfLoader.loadAsync('/assets/models/v40-30.glb')
   const house = setupModel(houseData)
-  house.traverse(n => {
+  house.traverse((n) => {
     if (n.isMesh) {
       if (n.material.name === 'esquadria.vidro') {
         n.castShadow = false
@@ -23,8 +23,8 @@ async function loadHouse() {
 
   const box = new Box3().setFromObject(house)
   const center = box.getCenter(new Vector3())
-  house.position.x += (house.position.x - center.x)
-  house.position.z += (house.position.z - center.z)
+  house.position.x += house.position.x - center.x
+  house.position.z += house.position.z - center.z
   // house.position.y = 2.2
 
   return { house }
